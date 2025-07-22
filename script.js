125dff249b3622bf58e5a1a0f99ed8a7175ba50a
David Groß <d.g1r2o3s4s@gmail.com>
2025-07-22 19:52:05 +0200

    feat: adden function showMainContent() and changed getPokemon() to get more stats

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-
-
-
-
-let countShownPokemon = 6;
+let countShownPokemon = 9;
 let shownPokemon = [];
-
 const BASE_URL = `https://pokeapi.co/api/v2/pokemon?limit=${countShownPokemon}&offset=0`;
 
-let dialogRef = document.getElementById('overlay');
+let overlayRef = document.getElementById('overlay');
+
 
 function init() {
     getPokemon();
 }
-
-
 
 async function getPokemon() {
     let response = await fetch(BASE_URL);
@@ -22,29 +16,30 @@ async function getPokemon() {
     for (let index = 0; index < countShownPokemon; index++) {
         let response2 = await fetch(responeToJson["results"][index]["url"]);
         let response2ToJson = await response2.json();
+         console.log(response2ToJson)
         shownPokemon.push(
             {
                 "name": response2ToJson['name'],
-                "img" : response2ToJson['sprites']['other']['home']['front_default'],
-                "types": getTypes(response2ToJson)
+                "img": response2ToJson['sprites']['other']['home']['front_default'],
+                "types": getTypes(response2ToJson),
+                "height": (response2ToJson['height']) / 10,
+                "weight": (response2ToJson['weight']) /10,
+                "abilities": response2ToJson['abilities'],
+                "base_experience": response2ToJson['base_experience']
             }
         )
     }
     renderPokemon();
     // console.log(shownPokemon);
+
 }
 
 function getTypes(response2ToJson) {
-    let currentTypes = [];
+    let currentTypes = "";
     for (let index = 0; index < response2ToJson['types'].length; index++) {
-        currentTypes.push(
-            {
-                "name": response2ToJson['types'][index]['type']['name']
-            }
-        );  
+        currentTypes += response2ToJson['types'][index]['type']['name'] + " ";
     }
-    console.log(currentTypes);
-    
+    currentTypes = currentTypes.trimEnd();
     return currentTypes;
 }
 
@@ -52,16 +47,27 @@ function renderPokemon() {
     let contentCardsRef = document.getElementById('content_cards');
     contentCardsRef.innerHTML = "";
     for (let index = 0; index < shownPokemon.length; index++) {
-        contentCardsRef.innerHTML += getContentCardsTemplate(index); 
+        contentCardsRef.innerHTML += getContentCardsTemplate(index);
     }
 }
 
 function openOverlay(index) {
-    
-    dialogRef.classList.remove('d_none');
-    dialogRef.innerHTML = getOverlayTemplate(index);
+    overlayRef.classList.remove('d_none');
+    overlayRef.innerHTML = getOverlayTemplate(index);
 }
 
 function closeOverlay() {
-    dialogRef.classList.add('d_none');
+    overlayRef.classList.add('d_none');
 }
+
+function showMainContent(index) {
+    let descriptionContentRef = document.getElementById(`content_description_${index}`);
+    descriptionContentRef.innerHTML = "";
+    let abilitiesString = "";
+    for (let abilityIndex = 0; abilityIndex < shownPokemon[index].abilities.length; abilityIndex++) {
+        abilitiesString += `${abilityIndex +1}: ` + shownPokemon[index].abilities[abilityIndex]['ability']['name'] + " <br>" ;
+    }
+    abilitiesString = abilitiesString.trimEnd();
+    descriptionContentRef.innerHTML = getMainTemplates(index, abilitiesString);
+}
+
